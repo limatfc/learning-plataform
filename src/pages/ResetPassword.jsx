df8e08bb4ad6f9493b860b4e9f10ea8f83ad1d9f
Thaisa Lima <thaisafc.lima@outlook.com>
2022-04-27 17:45:51 +0200

    reset page

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,3 +1,24 @@
+import { useState } from "react";
+import InputField from "../components/InputField";
+import inputData from "../data/inputFields.json";
+
 export default function ResetPassword() {
-  return <div>Reset</div>;
+  const [email, setEmail] = useState("");
+  const check = (item) => item !== "";
+
+  const info = inputData.resetPassword;
+
+  return (
+    <div>
+      <h2>Having problems with your password?</h2>
+      <h3>
+        To redefine your password, please type in all the information required
+        bellow:
+      </h3>
+      <form>
+        <InputField setup={info.email} actions={[setEmail, check]} />
+        <button>Reset password</button>
+      </form>
+    </div>
+  );
 }
