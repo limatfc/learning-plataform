ec737142491275c4ba663a436fae3e0a38877b32
Thaisa Lima <thaisafc.lima@outlook.com>
2022-04-30 09:35:29 +0200

    fixed StudentCourse update functionality

--- src/pages/StudentDashboard.jsx
+++ src/pages/StudentDashboard.jsx
@@ -1,23 +1,11 @@
-import { useEffect, useState } from "react";
 import CourseCard from "../components/CourseCard";
-import { getCollection } from "../scripts/firebase/fireStore";
+import useReadData from "../hooks/useReadData";
 import useUserProvider from "../store/useUserProvider";
 import Loading from "./Loading";
 
 export default function StudentDashboard() {
-  const [status, setStatus] = useState(null);
   const { coursesHandler, courses } = useUserProvider();
-
-  useEffect(() => {
-    async function readData() {
-      setStatus(0);
-      const courses = await getCollection("courses");
-      coursesHandler(courses);
-      setStatus(1);
-    }
-    readData();
-  }, [coursesHandler]);
-
+  const { status } = useReadData(coursesHandler, "courses");
   const courseCard = courses.map((course) => (
     <CourseCard course={course} key={course.id} />
   ));
