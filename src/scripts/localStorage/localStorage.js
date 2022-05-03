49681ac404ba6127a1960177591105957855b930
Thaisa Lima <thaisafc.lima@outlook.com>
2022-05-03 15:40:32 +0200

    fixed login

--- src/scripts/localStorage/localStorage.js
+++ src/scripts/localStorage/localStorage.js
@@ -1,7 +1,13 @@
+const localStorageKey = "userUID";
+
 export function setLocalStorage(key, uid) {
   return localStorage.setItem(key, uid);
 }
 
 export function getLocalStorage(key) {
   return localStorage.getItem(key);
+}
+
+export function removeItem() {
+  localStorage.removeItem(localStorageKey);
 }
